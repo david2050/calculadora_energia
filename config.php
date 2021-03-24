4a8fb9ac5304f2bcb20903aa7b3aeac4a1e9b38e
david2050 <david@virtualsolution.com.br>
2021-03-24 13:04:35 -0300

    configuration explicado

--- config.php
+++ config.php
@@ -1,7 +1,13 @@
 <?php
+
+
+//Configure com valores da sua distribuidora de energia. valor citado da enel SP tarifação minima FEV/2021
 $tarifa_enel_dist = 0.28551;
 $tarifa_enel_energia = 0.24868;
 $tarifa = $tarifa_enel_dist+$tarifa_enel_energia;
 
+//Horas de uso do equipamento no dia
 $horas_uso_diario = 12;
+
+// Dias de uso no mes
 $dias_uso_mes = 30;
