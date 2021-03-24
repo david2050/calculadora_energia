697caf66e6f618b811f0adfa40fdcbdfce786d52
david2050 <david@virtualsolution.com.br>
2021-03-24 13:04:47 -0300

    calculadora de energia

---
+++ calculadora_energia.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__.'/config.php';
+if(@!isset($argv[1])){
+    echo "\n É necessário informar os watts para calcular. Ex.: calculadoraenergia.php 20.8 \n";
+    exit;
+}
+$watts = $argv[1];
+$calculo_kwh_hora = number_format((($watts*1)/1000)*$tarifa, 2, ',', '.');
+echo "GASTO EM REAIS POR HORA: R$".$calculo_kwh_hora."\n CONTA SIMPLIFICADA: \n";
+
+$calculo_kwh_dia = (($watts*$horas_uso_diario)/1000);
+echo "KWH DIA: ".$calculo_kwh_dia."\n";
+$calculo_mes = $calculo_kwh_dia*$dias_uso_mes;
+echo "KWH MES: ".$calculo_mes."\n";
+$valor_total_reais = number_format($calculo_mes*$tarifa, 2, ',', '.');
+echo "VALOR EM REAIS MES: R$".$valor_total_reais."\n";
